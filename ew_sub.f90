175cd47f4592739a5e349dce7cdcc011d0acca1f
jhmatthews <jimim@hotmail.co.uk>
2016-08-02 18:49:16 +0100

    some comments in ew_sub.f90

--- ew_sub.f90
+++ ew_sub.f90
@@ -1,6 +1,23 @@
 !     file ew_sub.f
 !     compile with: f2py -c -m ew_sub ew_sub.f
-!     This file contains subroutines for generating mock angles on the sky
+
+! -------------------------------------------------
+!     function get_mock_angles
+!     This function generates mock angles for quasars. 
+!     Arguments:
+!       costhetas     array-like, float
+!                     the array that holds the cosines of the angles 
+!       bal_flags     array-like, int32
+!                     1 for a BALQSO, 0 for a quasar
+!       thetamin      float. thetamin
+!       max_angle     float. thetamax
+!       fluxes        array-like, float. fluxes of quasars.
+!       flux_limit    float. Single value flux limit for rejection limit.
+!       npts, nfluxes ints. Lengths of costhetas and fluxes respectively.
+! 
+!     note: be careful with placing the integer array lengths, as f2py will 
+!     move them to the end eithout telling the user.
+! -------------------------------------------------
 
       function get_mock_angles (costhetas, bal_flags, threshold,&
                                  &max_angle, fluxes, flux_limit, npts, nfluxes)
@@ -28,22 +45,28 @@
 !       apply the detection limit
         do while (undetected)
 
+          ! random number in costheta space       
           call random_number(costheta)
+
+          ! convert to angle in degrees
           theta = dacos(costheta) * 180.d0 / PI
 
-!          write(*,*) costheta, theta, max_angle
-
+          ! does this mock object survive the flux limit?
           call is_source_detected(costheta, undetected,&
                                   &fluxes, flux_limit, nfluxes)
 
+          ! is the object "obscured"?
           if (theta .gt. max_angle) then
             undetected = .TRUE.
           endif
 
         enddo
 
+        ! if we get here then we've survived the various tests
+        ! so copy the value over to the array
         costhetas(i) = costheta
 
+        ! flag if this object is a BALQSO or not
         if (theta .gt. threshold) then
           bal_flags(i) = 1
         else
@@ -51,11 +74,20 @@
         endif
 
 10    enddo
+      return
       end
 
-
-
-
+! -------------------------------------------------
+!     function get_mock_angles
+!     This function generates mock angles for quasars. 
+!     Arguments:
+!       costheta      float. cosine of angle in question
+!       undetected    bool. should be false on entry 
+!                     and is modified by this routine
+!       fluxes        array-like, float. fluxes of quasars.
+!       flux_limit    float. Single value flux limit for rejection limit.
+!       nfluxes       int.  Length of fluxes.
+! -------------------------------------------------
 
       subroutine is_source_detected(costheta, undetected, fluxes,&
                                     &flux_limit, nfluxes)
